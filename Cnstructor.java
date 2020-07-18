c98be63abc637e517ee5709690503b110ada2823
SanaFatama587561 <65457094+SanaFatama587561@users.noreply.github.com>
2020-07-18 20:26:23 +0530

    Add files via upload

---
+++ Cnstructor.java
@@ -0,0 +1,13 @@
+package Javabasics;
+
+public class Cnstructor {       //step -3
+	public Cnstructor() {       // step -4
+		System.out.println("Hi,I am Constructor");  // step -5 and print 
+	}
+	
+public static void main(String [] args) {   //program will start from here  - Step 1
+	Cnstructor obj1 = new Cnstructor();     // Step -2
+	
+	
+}
+}
